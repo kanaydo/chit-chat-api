7c54066081248546e599a7861398f91594cd02dc
Batara Girsang <batara.girsang@gmail.com>
2019-10-15 09:56:24 +0700

    add authentication module for managing authentication methods

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -9,7 +9,7 @@ class JsonWebToken
 
   def self.decode(token)
     decoded = JWT.decode(token, SECRET_KEY).first
-    HashWithIndifferentAccess.new decod
+    HashWithIndifferentAccess.new decoded
   end
 
 end
